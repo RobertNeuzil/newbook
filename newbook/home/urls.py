0c02b4e87d95ec3f90ea4149ca8d3ea03d341b36
robert neuzil <robertneuzil@gmail.com>
2018-10-16 16:51:54 -0400

    added index + sign + css styles

--- newbook/home/urls.py
+++ newbook/home/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
 
-    path('', views.index, name="index")
-]
+    path('', views.index, name="index"),
+    path('sign/', views.sign, name="sign"),
+    	
+    	]
--- newbook/home/views.py
+++ newbook/home/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 def index(request):
 	return render(request, "home/index.html")
 
+def sign(request):
+	return render(request, "home/sign.html")
+
 # Create your views here.
