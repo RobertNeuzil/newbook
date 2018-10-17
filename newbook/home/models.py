9b8a4b53fd252527593ae317170a8b96c33d04be
robert neuzil <robertneuzil@gmail.com>
2018-10-17 14:24:01 -0400

    built models

--- newbook/home/models.py
+++ newbook/home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+
+class Comment(models.Model):
+	name = models.CharField(max_length=20)
+	comment = models.TextField()
+	date_added = models.DateTimeField(default=timezone.now)
+
+	def __str__(self):
+		return f" {self.name} "
+
 
 # Create your models here.
