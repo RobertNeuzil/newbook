17a12d9f70f87c336f06104d8866ced72bede523
robert neuzil <robertneuzil@gmail.com>
2018-10-17 16:21:30 -0400

    completed project

--- newbook/home/views.py
+++ newbook/home/views.py
@@ -2,9 +2,10 @@
 ''' The First "Comments"in the context dic is the key the value is the actual list of comments  min 49'''
 
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Comment
+from .forms import CommentForm
 
 def index(request):
 	comments = Comment.objects.order_by("-date_added")
@@ -19,6 +20,28 @@ def index(request):
 	return render(request, "home/index.html", context)
 
 def sign(request):
-	return render(request, "home/sign.html")
+
+	if request.method == "POST":
+		form = CommentForm(request.POST)
+
+		if form.is_valid():
+
+			new_comment = Comment(name=request.POST['name'], comment= request.POST['comment'])
+			new_comment.save()
+			return redirect ('index')
+
+	
+	else:
+
+
+
+		form = CommentForm()
+	
+
+	context = {
+	"form" : form
+	}
+	
+	return render(request, "home/sign.html", context)
 
 # Create your views here.
